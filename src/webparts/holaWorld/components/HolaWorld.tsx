a83702c8f27a0d413de4a7a9566392f7173a18fe
sudharshan.shanmugam tridentsqa.com <sudharshan.shanmugam@tridentsqa.com>
2026-08-20 14:58:23 +0530

    Redesign web part as a welcome hero screen

--- src/webparts/holaWorld/components/HolaWorld.tsx
+++ src/webparts/holaWorld/components/HolaWorld.tsx
@@ -1,14 +1,19 @@
 import * as React from 'react';
 import styles from './HolaWorld.module.scss';
 import type { IHolaWorldProps } from './IHolaWorldProps';
+import { escape } from '@microsoft/sp-lodash-subset';
 
 export default class HolaWorld extends React.Component<IHolaWorldProps> {
   public render(): React.ReactElement<IHolaWorldProps> {
-    const { hasTeamsContext } = this.props;
+    const { hasTeamsContext, userDisplayName, environmentMessage } = this.props;
 
     return (
       <section className={`${styles.holaWorld} ${hasTeamsContext ? styles.teams : ''}`}>
-        senior logadheep
+        <div className={styles.welcomeCard}>
+          <div className={styles.badge}>senior logadheep</div>
+          <h1 className={styles.title}>Welcome{userDisplayName ? `, ${escape(userDisplayName)}` : ''}</h1>
+          {environmentMessage && <p className={styles.subtitle}>{environmentMessage}</p>}
+        </div>
       </section>
     );
   }
