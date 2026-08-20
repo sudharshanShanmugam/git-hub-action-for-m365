c5320d6118c85eb1881ee3c5b54d028b905e66da
sudharshan.shanmugam tridentsqa.com <sudharshan.shanmugam@tridentsqa.com>
2026-08-20 15:04:50 +0530

    Update badge text to lead logadheep

--- src/webparts/holaWorld/components/HolaWorld.tsx
+++ src/webparts/holaWorld/components/HolaWorld.tsx
@@ -10,7 +10,7 @@ export default class HolaWorld extends React.Component<IHolaWorldProps> {
     return (
       <section className={`${styles.holaWorld} ${hasTeamsContext ? styles.teams : ''}`}>
         <div className={styles.welcomeCard}>
-          <div className={styles.badge}>senior logadheep</div>
+          <div className={styles.badge}>lead logadheep</div>
           <h1 className={styles.title}>Welcome{userDisplayName ? `, ${escape(userDisplayName)}` : ''}</h1>
           {environmentMessage && <p className={styles.subtitle}>{environmentMessage}</p>}
         </div>
